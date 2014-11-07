60abd6158508f3d4793a75160ed7ca7ec8916caf
Juan Lebrijo <juan@lebrijo.com>
2014-11-08 00:48:09 +0100

    cap backup:restore commmand

--- lib/prun-ops/cap/backup.rb
+++ lib/prun-ops/cap/backup.rb
@@ -1,6 +1,13 @@
-desc ''
-task :backup, :tag
-
 namespace :backup do
-  task :restore, :tag
+  desc 'Restore data from git repo'
+  task :restore, :tag do |task, args|
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:stage) do
+          execute :rake, "backup:restore #{args[:tag]}"
+        end
+      end
+    end
+
+  end
 end

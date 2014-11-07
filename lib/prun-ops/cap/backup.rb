4f59f141e6b0dbfc5ff7b0763667db9edb5fe07a
Juan Lebrijo <juan@lebrijo.com>
2014-11-07 14:52:48 +0100

    create 'cap data:pull' command

--- lib/prun-ops/cap/backup.rb
+++ lib/prun-ops/cap/backup.rb
@@ -1,7 +1,16 @@
+namespace :data do
+  desc 'Pull data (db/files) from remote (i.e: production) application.'
+  task :pull do
+    invoke "files:pull"
+    invoke "db:pull"
+  end
+end
+
 namespace :db do
   desc 'Pull db'
   task :pull do
     on roles(:app) do |host|
+      debug ":   Pulling database from #{fetch(:stage)} ..."
       within "#{current_path}/tmp" do
         with rails_env: :production do
           rake "db:backup"
@@ -21,6 +30,7 @@ namespace :files do
   task :pull do
     on roles(:app) do |host|
       run_locally do
+        debug ":   Pulling Files from #{fetch(:stage)} ..."
         if fetch(:backup_dirs).any?
           fetch(:backup_dirs).each do |dir|
             execute "scp -r -P #{host.port} #{host.user}@#{host.hostname}:#{current_path}/#{dir} #{dir}"
